502f6d93e5f3926e7eb239347888a38cd110ed48
GuguuHub <agustinniederer@gmail.com>
2026-06-11 20:02:47 -0300

    Update registrar_venta.py

--- BORRADORES/registrar_venta.py
+++ BORRADORES/registrar_venta.py
@@ -4,10 +4,9 @@ from Nuevofunciones import *
 def registrar_venta(lst_titulos, lst_entradas, lst_valores, lst_estados, lst_stock):
     '''Registra ventas de entradas. El usuario podrá seleccionar una película y la cantidad de entradas a vender'''
 
-    i = ingresar_titulo(
-        "Seleccione una pelicula: ",
-        lst_titulos
-    )
+    while i < 0 or i >= len(lst_titulos):
+        print("Película inexistente.")
+        i = int(input("Seleccione una película de la lista: "))
 
     if lst_estados[i] == "Finalizada":
         print("No se pueden vender entradas de una pelicula finalizada.")
