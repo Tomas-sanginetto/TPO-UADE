eeba7666b9c72b5e87a156ffc3773b9f44913b6b
GuguuHub <agustinniederer@gmail.com>
2026-06-11 20:39:54 -0300

    Update registrar_venta.py

--- BORRADORES/registrar_venta.py
+++ BORRADORES/registrar_venta.py
@@ -1,32 +1,24 @@
-
-from Nuevofunciones import *
-
 def registrar_venta(lst_titulos, lst_entradas, lst_valores, lst_estados, lst_stock):
     '''Registra ventas de entradas. El usuario podrá seleccionar una película y la cantidad de entradas a vender'''
 
-    i = ingresar_titulo(
-        "Seleccione una pelicula: ",
-        lst_titulos
-    )
-    
-    if lst_estados[i] == "Finalizada":
+    indice = ingresar_titulo("Seleccione una pelicula:", lst_titulos)
+
+    if lst_estados[indice] == "Finalizada":
         print("No se pueden vender entradas de una pelicula finalizada.")
         return
 
-    cantidad = ingresar_positivo(
-        "Ingrese cantidad de entradas a vender: "
-    )
+    cantidad = ingresar_positivo("Ingrese cantidad de entradas a vender: ")
 
-    if cantidad > lst_stock[i]:
+    if cantidad > lst_stock[indice]:
         print("No hay stock suficiente.")
-        print("Stock disponible: ", lst_stock[i])
+        print("Stock disponible: ", lst_stock[indice])
         return
 
-    total = cantidad * lst_valores[i]
+    total = cantidad * lst_valores[indice]
 
     print("Total a pagar: $", total)
 
-    lst_stock[i] = lst_stock[i] - cantidad
-    lst_entradas[i] = lst_entradas[i] + cantidad
+    lst_stock[indice] = lst_stock[indice] - cantidad
+    lst_entradas[indice] = lst_entradas[indice] + cantidad
 
     print("Venta registrada correctamente.")
